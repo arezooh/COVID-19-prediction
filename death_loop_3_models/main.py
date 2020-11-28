ef522fa74be70d0bc43524a2790729c03fad2312
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2020-11-28 23:02:29 +0330

    update

--- death_loop_3_models/main.py
+++ death_loop_3_models/main.py
@@ -7,11 +7,18 @@ def Run(r, test_point):
     subprocess.call("python ./" + str(r) + "/prediction.py " + str(test_point), shell=True)
 
 def main():
-    for r in range(1, 11):
-        test_point = 10 - r + int(argv[1])
-        print(100*'*')
-        print('r =', r, ', test_point =', test_point)
-        Run(r, test_point)
+    if int(argv[1]) >= 0:
+        for r in range(1, 11):
+            test_point = 10 - r + int(argv[1])
+            print(100*'*')
+            print('r =', r, ', test_point =', test_point)
+            Run(r, test_point)
+    else:
+        for r in range(1, 11 + int(argv[1])):
+            test_point = 10 - r + int(argv[1])
+            print(100*'*')
+            print('r =', r, ', test_point =', test_point)
+            Run(r, test_point)
 
 
 if __name__ == "__main__":
