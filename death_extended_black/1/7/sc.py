2e051c3ccbb745a3afa14e53b76e46d7b7478c7a
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2020-11-30 20:01:25 +0330

    add death extended_black

---
+++ death_extended_black/1/7/sc.py
@@ -0,0 +1,23 @@
+from sys import argv
+import sys
+import os
+import subprocess
+from pexecute.process import ProcessLoom
+
+base_r = 1
+r = 7
+
+def Run(i):
+    subprocess.call("python ./prediction.py "+str(i), shell=True)
+
+def main():
+    for i in range(19 - base_r - r + 7):
+        print(200 * '*')
+        print(i)
+        Run(i)
+
+
+
+if __name__ == "__main__":
+
+    main()
